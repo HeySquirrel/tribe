a2baa4cefbda8210ce41fcd6b53debc2473fd2bf
Esterline <adam@esterlines.com>
2017-09-10 19:32:26 -0600

    Remove unused method

--- app/table_views.go
+++ app/table_views.go
@@ -99,27 +99,6 @@ func (a *App) UpdateContributors2(contributors []*git.Contributor) {
 	})
 }
 
-func (a *App) UpdateContributors(contributors []*git.Contributor) {
-	a.updateView(contributorsView, func(v *gocui.View) {
-		maxX, _ := v.Size()
-
-		table := tablewriter.NewWriter(v)
-		table.SetColWidth(maxX)
-		table.SetHeader([]string{"Name", "Commits", "Last Commit"})
-		table.SetBorder(false)
-
-		for _, contributor := range contributors {
-			table.Append([]string{
-				contributor.Name,
-				strconv.Itoa(contributor.Count),
-				humanize.Time(contributor.LastCommit),
-			})
-		}
-
-		table.Render()
-	})
-}
-
 func (a *App) UpdateRelatedFiles(files []*git.RelatedFile) {
 	a.updateView(associatedFilesView, func(v *gocui.View) {
 		maxX, _ := v.Size()
