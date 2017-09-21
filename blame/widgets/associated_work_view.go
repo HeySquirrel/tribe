c84766ab0d9c89f793e6b79930ef9d15a8317155
Esterline <adam@esterlines.com>
2017-09-21 11:11:20 -0600

    Move associated above frequent contributors

--- blame/widgets/associated_work_view.go
+++ blame/widgets/associated_work_view.go
@@ -25,9 +25,9 @@ func (a *AssociatedWorkView) Layout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
 
 	x1 := int(0.0 * float64(maxX))
-	y1 := int(0.75 * float64(maxY))
+	y1 := int(0.5 * float64(maxY))
 	x2 := int(0.5*float64(maxX)) - 1
-	y2 := int(1.0*float64(maxY)) - 1
+	y2 := int(0.75*float64(maxY)) - 1
 
 	v, err := g.SetView(a.name, x1, y1, x2, y2)
 	if err != gocui.ErrUnknownView {
--- blame/widgets/frequent_contributors_view.go
+++ blame/widgets/frequent_contributors_view.go
@@ -26,9 +26,9 @@ func (c *FrequentContributorsView) Layout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
 
 	x1 := int(0.0 * float64(maxX))
-	y1 := int(0.5 * float64(maxY))
+	y1 := int(0.75 * float64(maxY))
 	x2 := int(0.5*float64(maxX)) - 1
-	y2 := int(0.75*float64(maxY)) - 1
+	y2 := int(1.0*float64(maxY)) - 1
 
 	v, err := g.SetView(c.name, x1, y1, x2, y2)
 	if err != gocui.ErrUnknownView {
