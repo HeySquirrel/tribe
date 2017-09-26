075bbcb8bfcb92b07050d269b731329b9555c2ae
Esterline <adam@esterlines.com>
2017-09-26 08:59:29 -0600

    Removed a unneeded comment

--- cmd/root.go
+++ cmd/root.go
@@ -51,10 +51,6 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports Persistent Flags, which, if defined here,
-	// will be global for your application.
-
 	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tribe.json)")
 	RootCmd.PersistentFlags().String("rally", "", "API key for Rally Webservice Access")
 
